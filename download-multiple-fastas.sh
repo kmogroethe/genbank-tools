c1be76e48808bde874799a210b025ed84b589d23
Kmoser <moserk9@gmail.com>
2020-02-12 21:37:13 -0500

    upload script

---
+++ download-multiple-fastas.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+###############################################################################
+
+# kamoser
+# Feb 2020
+
+# ABOUT
+
+# INPUT: list of genbank ids (one per line)
+# OUTPUT: nucleotide fasta files for each genbank id number
+
+# run as ./script <list> (see below)
+
+###############################################################################
+
+list=$1
+
+while IFS=$'\n' read -r id; do
+  id+=($id); 
+done < ${list}
+
+for acn in "${id[@]}"; do 
+  curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&amp;id=${acn}&amp;rettype=fasta" \
+  > ${acn}.fasta
+done
+
+###############################################################################
+
+echo "Read through script. Good job you."
+
+###############################################################################
